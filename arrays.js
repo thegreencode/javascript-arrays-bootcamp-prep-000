20993600c7833e85bac1eaeb599df43ee8f396de
Jessica Green <jgreen629@yahoo.com>
2018-04-13 10:46:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,8 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
   
 
 function addElementToBeginningOfArray(array, element) {
-   
-   array = ['foo', 1]
+  array = ['foo', 1]
 }
 
 console.log(array)
